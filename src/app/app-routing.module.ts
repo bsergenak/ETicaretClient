f7e1952426f2da4db70d8b273134426f3f10d3e9
bsergenak <berkaysergenak@hotmail.com>
2023-05-30 03:40:36 +0300

    Kullanici kayit formu tasarlama(reactive form)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,21 +5,22 @@ import { LayoutComponent } from './admin/layout/layout.component';
 import { HomeComponent } from './ui/components/home/home.component';
 
 const routes: Routes = [
-  {
-    path: "admin", component: LayoutComponent, children: [
-      { path: "", component: DashboardComponent },
-      { path: "customers", loadChildren: () => import("./admin/components/customer/customer.module").then(module => module.CustomerModule) },
-      { path: "products", loadChildren: () => import("./admin/components/products/products.module").then(module => module.ProductsModule) },
-      { path: "orders", loadChildren: () => import("./admin/components/order/order.module").then(module => module.OrderModule) },
-    ]
-  },
-  { path: "", component: HomeComponent },
-  { path: "basket", loadChildren: () => import("./ui/components/baskets/baskets.module").then(module => module.BasketsModule) },
-  { path: "products", loadChildren: () => import("./ui/components/products/products.module").then(module => module.ProductsModule) },
+    {
+        path: "admin", component: LayoutComponent, children: [
+            { path: "", component: DashboardComponent },
+            { path: "customers", loadChildren: () => import("./admin/components/customer/customer.module").then(module => module.CustomerModule) },
+            { path: "products", loadChildren: () => import("./admin/components/products/products.module").then(module => module.ProductsModule) },
+            { path: "orders", loadChildren: () => import("./admin/components/order/order.module").then(module => module.OrderModule) },
+        ]
+    },
+    { path: "", component: HomeComponent },
+    { path: "basket", loadChildren: () => import("./ui/components/baskets/baskets.module").then(module => module.BasketsModule) },
+    { path: "products", loadChildren: () => import("./ui/components/products/products.module").then(module => module.ProductsModule) },
+    { path: "register", loadChildren: () => import("./ui/components/register/register.module").then(module => module.RegisterModule) }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
