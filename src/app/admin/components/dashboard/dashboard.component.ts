73aa0311c0bb32cb7145734f1a49eeff9c06c181
bsergenak <berkaysergenak@hotmail.com>
2023-06-12 17:25:38 +0300

    Mimarisel Olarak SignalR Altyapı Entegrasyonu

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -3,33 +3,42 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 
 import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { HubUrls } from '../../../constants/hub-urls';
+import { ReceiveFunctions } from '../../../constants/receive-functions';
+import { SignalrService } from '../../../services/common/signalr.service';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent extends BaseComponent implements OnInit {
 
-  constructor(private alertify: AlertifyService, spinner: NgxSpinnerService) {
-    super(spinner)
-  }
+    constructor(private alertify: AlertifyService, spinner: NgxSpinnerService, private signalRService: SignalrService) {
+        super(spinner)
+        signalRService.start(HubUrls.ProductHub)
+    }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
+        this.signalRService.on(ReceiveFunctions.ProductAddedMessageReceiveFunction, message => {
+            this.alertify.message(message, {
+                messageType: MessageType.Notify,
+                position: Position.TopRight
+            })
+        });
+    }
 
-  }
+    m() {
 
-  m() {
+        this.alertify.message("Merhaba", {
+            messageType: MessageType.Success,
+            delay: 5,
+            position: Position.TopRight
+        })
+    }
 
-    this.alertify.message("Merhaba", {
-      messageType: MessageType.Success,
-      delay: 5,
-      position: Position.TopRight
-    })
-  }
-
-  d() {
-    this.alertify.dismiss();
-  }
+    d() {
+        this.alertify.dismiss();
+    }
 
 }
