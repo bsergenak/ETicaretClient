413e58aa3c077e3ddefcfc8e5e6b856796487a45
bsergenak <berkaysergenak@hotmail.com>
2023-08-15 00:46:11 +0300

    Rol Yönetim Sayfasının Oluşturulması

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from './layout/layout.module';
 import { ComponentsModule } from './components/components.module';
+import { CreateComponent } from './componenets/role/create/create.component';
+import { ListComponent } from './componenets/role/list/list.component';
 
 
 
 @NgModule({
-    declarations: [],
+    declarations: [
+    CreateComponent,
+    ListComponent
+  ],
     imports: [
         CommonModule,
         LayoutModule,
