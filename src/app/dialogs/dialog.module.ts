869533783f6c71b625688498f8751f3fc4f813d2
bsergenak <berkaysergenak@hotmail.com>
2023-09-04 02:31:26 +0300

    QrCode Okuma ve Ürün Stock Bilgisi Güncelleme

--- src/app/dialogs/dialog.module.ts
+++ src/app/dialogs/dialog.module.ts
@@ -19,6 +19,10 @@ import { MatListModule } from '@angular/material/list';
 import { AuthorizeMenuDialogComponent } from './authorize-user-dialog/authorize-user-dialog.component';
 import { AuthorizeUserDialogComponent } from './authorize-menu-dialog/authorize-menu-dialog.component';
 import { QrcodeDialogComponent } from './qrcode-dialog/qrcode-dialog.component';
+import { QrcodeReadingDialogComponent } from './qrcode-reading-dialog/qrcode-reading-dialog.component';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { NgxScannerQrcodeModule } from 'ngx-scanner-qrcode';
 
 @NgModule({
     declarations: [
@@ -30,12 +34,14 @@ import { QrcodeDialogComponent } from './qrcode-dialog/qrcode-dialog.component';
         CompleteOrderDialogComponent,
         AuthorizeMenuDialogComponent,
         AuthorizeUserDialogComponent,
-        QrcodeDialogComponent
+        QrcodeDialogComponent,
+        QrcodeReadingDialogComponent
     ],
     imports: [
         CommonModule,
         MatDialogModule, MatButtonModule, MatCardModule, MatTableModule, MatToolbarModule,
-        FileUploadModule, MatBadgeModule, MatListModule
+        FileUploadModule, MatBadgeModule, MatListModule, MatFormField, MatInputModule,
+        NgxScannerQrcodeModule
     ]
 })
 export class DialogModule { }
